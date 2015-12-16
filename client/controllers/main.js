2b8df203ab39ddd63f1ed04e381db0ec71a6445f
brandon932 <bthornton_932@yahoo.com>
2015-12-15 23:21:53 -0700

    updated project for demo day

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -21,8 +21,17 @@ app.controller('mainCtrl', function($scope, $rootScope, $location, httpFactory, 
     // $location.path("/user");
 
   };
-
-
+  $scope.flyDrone = function(drone){
+    console.log("test flight flyig drone" + drone.name);
+    $location.path("/webFlight");
+    $window.localStorage.currentDrone = JSON.stringify(drone);
+  };
+  $scope.done = function(){
+    var drone =JSON.parse(localStorage.getItem('currentDrone'));
+    $location.path("/home");
+    httpFactory.put('api/drone/'+ drone._id , {rented:false});
+    delete $window.localStorage.currentDrone;
+  };
 
 
   $scope.getDrones();
